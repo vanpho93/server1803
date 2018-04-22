912b41e93eaf76820a5ccb3623bf13f1f8a669b7
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-22 09:51:57 +0700

    password

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const { json } = require('body-parser');
+const { hash } = require('bcryptjs');
 require('./connectDatabase');
 const { User } = require('./User');
 
 const app = express();
 app.use(json());
 
-app.post('/signup', (req, res) => {
-    const { name, email, password } = req.body;
-    const user = new User({ name, email, password });
-    user.save()
-    .then(() => res.send({ success: true, user }))
-    .catch(error => res.status(400).send({ success: false, error: error.message }));
+app.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const encryptedPassword = await hash(password, 8);
+        const user = new User({ name, email, password: encryptedPassword });
+        await user.save();
+        const userInfo = user.toObject();
+        delete userInfo.password;
+        res.send({ success: true, user: userInfo });
+    } catch (error) {
+        res.status(400).send({ success: false, error: error.message });
+    }
 });
 
 app.post('/signin', (req, res) => {});
