2a1d30ed2704eb786e49f433718c41082b4bfae3
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-22 09:59:43 +0700

    sign in

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { json } = require('body-parser');
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 require('./connectDatabase');
 const { User } = require('./User');
 
@@ -21,7 +21,20 @@ app.post('/signup', async (req, res) => {
     }
 });
 
-app.post('/signin', (req, res) => {});
+app.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('Cannot find user.');
+        const same = await compare(password, user.password);
+        if (!same) throw new Error('Cannot find user.');
+        const userInfo = user.toObject();
+        delete userInfo.password;
+        res.send({ success: true, user: userInfo });
+    } catch (error) {
+        res.status(400).send({ success: false, error: error.message });
+    }
+});
 
 // app.post('/check', (req, res) => {});
 app.listen(3000, () => console.log('Server started.'));
